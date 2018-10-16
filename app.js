f0e712ffe6928dee477390144693d7775ed8d67b
depauna <34343404+depauna@users.noreply.github.com>
2018-10-16 15:16:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
-app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/user1', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
