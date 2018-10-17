07e43d8331bf219168241fae92cd6e890cfdc664
depauna <34343404+depauna@users.noreply.github.com>
2018-10-17 10:49:38 +0200

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { catchError, tap, map } from 'rxjs/operators';
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
-const apiUrl = "/api";
+const apiUrl = "/user1/api";
 
 @Injectable({
   providedIn: 'root'
