5aeb075a692d10adb78adf65eb9366bc25c61acc
depauna <34343404+depauna@users.noreply.github.com>
2018-10-17 09:33:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
-app.use('/user2/', express.static(path.join(__dirname, 'dist/mean-angular6')));
-app.use('/user2/books', express.static(path.join(__dirname, 'dist/mean-angular6')));
-app.use('/user2/book-details/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
-app.use('/user2/book-create', express.static(path.join(__dirname, 'dist/mean-angular6')));
-app.use('/user2/book-edit/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/user1/', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/user1/books', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/user1/book-details/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/user1/book-create', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/user1/book-edit/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
